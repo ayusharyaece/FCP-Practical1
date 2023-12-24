0fe9cedd66fddf717286ed748826ef831ae5a71b
ayusharyaece <150252989+ayusharyaece@users.noreply.github.com>
2023-12-24 23:48:37 +0530

    Ques1.c

---
+++ Ques1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void addArrays(int r, int c, int a[r][c], int b[r][c], int res[r][c]) {
+    for (int i = 0; i < r; ++i)
+        for (int j = 0; j < c; ++j)
+            res[i][j] = a[i][j] + b[i][j];
+}
+
+void displayArray(int r, int c, int arr[r][c]) {
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j)
+            printf("%d\t", arr[i][j]);
+        printf("\n");
+    }
+}
+
+int main() {
+    int r = 3, c = 3;
+    int arr1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int arr2[3][3] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+    int result[3][3];
+
+    addArrays(r, c, arr1, arr2, result);
+
+    displayArray(r, c, arr1);
+    displayArray(r, c, arr2);
+    displayArray(r, c, result);
+
+    return 0;
+}
