e5cbc7c01ed8b5d7ec67444553e2fe711a1b5c68
ayusharyaece <150252989+ayusharyaece@users.noreply.github.com>
2023-12-25 22:25:53 +0530

    Ques4(b).c

--- Ques4(b).c
+++ Ques4(b).c
@@ -4,11 +4,10 @@
 int main() {
     char inputString[100];
 
-    // Input the string from the user
+ 
     printf("Enter a string: ");
     scanf("%s", inputString);
 
-    // Calculate and display the length of the string
     printf("Length of the string: %zu\n", strlen(inputString));
 
     return 0;
