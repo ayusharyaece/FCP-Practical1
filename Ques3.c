6381b4de5e44f285163f599569bf94b622c34872
ayusharyaece <150252989+ayusharyaece@users.noreply.github.com>
2023-12-25 22:18:15 +0530

    Ques3.c

--- Ques3.c
+++ Ques3.c
@@ -3,20 +3,20 @@
 void reverseString(char str[]) {
     int start = 0, end = 0;
 
-    // Find the end of the string
+    
     while (str[end] != '\0') {
         end++;
     }
     end--;
 
-    // Reverse the string
+
     while (start < end) {
-        // Swap characters at start and end positions
+      
         char temp = str[start];
         str[start] = str[end];
         str[end] = temp;
 
-        // Move start and end pointers towards each other
+      
         start++;
         end--;
     }
@@ -25,14 +25,14 @@ void reverseString(char str[]) {
 int main() {
     char inputString[100];
 
-    // Input the string from the user
+
     printf("Enter a string: ");
     scanf("%s", inputString);
 
-    // Reverse the string
+
     reverseString(inputString);
 
-    // Display the reversed string
+   
     printf("Reversed string: %s\n", inputString);
 
     return 0;
